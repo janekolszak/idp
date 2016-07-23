04e3f7c87f3a7be939ea5c80a1d758ff361b5514
Jan Olszak <janekolszak@gmail.com>
2016-07-23 18:14:00 +0200

    Periodically cleanup the validation table

--- userdb/rethinkdb/verifier/worker.go
+++ userdb/rethinkdb/verifier/worker.go
@@ -1,7 +1,6 @@
 package verifier
 
 import (
-	// "bytes"
 	"fmt"
 	"github.com/nu7hatch/gouuid"
 	r "gopkg.in/dancannon/gorethink.v2"
@@ -10,7 +9,7 @@ import (
 	"io"
 	"net/url"
 	"sync"
-	// "time"
+	"time"
 )
 
 // Data received from RethinkDB in the Change feed
@@ -20,31 +19,36 @@ type VerificationChange struct {
 }
 
 type WorkerOpts struct {
-	Session         *r.Session
-	Host            string
-	Port            int
-	User            string
-	Password        string
-	From            string
-	Subject         string
-	ContentType     string
-	EndpointAddress string
-	EmailTemplate   *template.Template
+	Session            *r.Session
+	Host               string
+	Port               int
+	User               string
+	Password           string
+	From               string
+	Subject            string
+	EndpointAddress    string
+	TextTemplate       *template.Template
+	HtmlTemplate       *template.Template
+	VerificationMaxAge time.Duration
+	CleanupInterval    time.Duration
 }
 
 // Gets Verifications and sends emails.
 // Should be spawned only in one instance.
+// Only one Worker can operate
 type Worker struct {
 	opt WorkerOpts
 
 	table  string
 	url    *url.URL
 	msg    *gomail.Message
+	data   map[string]string
 	dialer *gomail.Dialer
 
 	// Process control
-	ctrl      chan bool
-	waitGroup sync.WaitGroup
+	ctrl          chan bool
+	cleanupTicker *time.Ticker
+	waitGroup     sync.WaitGroup
 }
 
 func NewWorker(opt WorkerOpts) (*Worker, error) {
@@ -62,6 +66,17 @@ func NewWorker(opt WorkerOpts) (*Worker, error) {
 	w.msg = gomail.NewMessage()
 	w.msg.SetHeader("From", opt.From)
 	w.msg.SetHeader("Subject", opt.Subject)
+	if w.opt.TextTemplate != nil {
+		w.msg.AddAlternativeWriter("text/plain", func(writer io.Writer) error {
+			return w.opt.TextTemplate.Execute(writer, w.data)
+		})
+	}
+
+	if w.opt.HtmlTemplate != nil {
+		w.msg.AddAlternativeWriter("text/html", func(writer io.Writer) error {
+			return w.opt.HtmlTemplate.Execute(writer, w.data)
+		})
+	}
 
 	// Dialer will send emails
 	w.dialer = gomail.NewDialer(opt.Host, opt.Port, opt.User, opt.Password)
@@ -82,6 +97,8 @@ func (w *Worker) Start() error {
 	dataChannel := make(chan VerificationChange)
 	cursor.Listen(dataChannel)
 
+	w.cleanupTicker = time.NewTicker(w.opt.CleanupInterval)
+
 	w.ctrl = make(chan bool, 1)
 	w.waitGroup.Add(1)
 	go w.run(dataChannel)
@@ -92,6 +109,7 @@ func (w *Worker) Start() error {
 // Stops the Worker goroutine
 func (w *Worker) Stop() {
 	w.ctrl <- true
+	w.cleanupTicker.Stop()
 	w.waitGroup.Wait()
 }
 
@@ -116,6 +134,16 @@ func (w *Worker) run(dataChannel <-chan VerificationChange) {
 				// Shouldn't happen
 			}
 
+		case now, ok := <-w.cleanupTicker.C:
+			fmt.Println(now, ok)
+			if !ok {
+				// I have no idea why..
+				return
+			}
+			err := w.deleteExpired(now)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
 		case <-w.ctrl:
 			// Stopping
 			return
@@ -138,14 +166,11 @@ func (w *Worker) sendVerificationEmail(verification *Verification) error {
 	// <[uid]@[sendingdomain.com]>
 	w.msg.SetHeader("Message-Id", "<"+id.String()+"@"+w.url.Host+">")
 
-	w.msg.AddAlternativeWriter(w.opt.ContentType, func(writer io.Writer) error {
-		data := map[string]string{
-			"Username": verification.Username,
-			"Email":    verification.Email,
-			"URL":      w.url.String(),
-		}
-		return w.opt.EmailTemplate.Execute(writer, data)
-	})
+	w.data = map[string]string{
+		"Username": verification.Username,
+		"Email":    verification.Email,
+		"URL":      w.url.String(),
+	}
 
 	err = w.dialer.DialAndSend(w.msg)
 	if err != nil {
@@ -158,5 +183,11 @@ func (w *Worker) sendVerificationEmail(verification *Verification) error {
 		"sentCount":    r.Row.Field("sentCount").Add(1).Default(0),
 		"lastSentTime": r.Now()}).RunWrite(w.opt.Session)
 
+	return err
+}
+
+// Deletes expired entries
+func (w *Worker) deleteExpired(now time.Time) error {
+	_, err := r.Table(w.table).Between(r.MinVal, now.Add(-w.opt.VerificationMaxAge), r.BetweenOpts{Index: "lastSentTime"}).Delete().Run(w.opt.Session)
 	return err
 }
